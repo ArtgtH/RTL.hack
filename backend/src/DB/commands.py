b40d420a967a414edc239e535dee172463eee926
artem <artem2004920@gmail.com>
2024-09-03 16:37:09 +0300

    сделал неблокирующее "выполнение" задач

--- backend/src/DB/commands.py
+++ backend/src/DB/commands.py
@@ -5,67 +5,67 @@ from src.DB.models import User, Task
 
 
 class AsyncORM:
-	@staticmethod
-	async def recreate_tables():
-		async with async_engine.begin() as conn:
-			await conn.run_sync(Base.metadata.drop_all)
-			await conn.run_sync(Base.metadata.create_all)
+    @staticmethod
+    async def recreate_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
 
-	@staticmethod
-	async def create_tables():
-		async with async_engine.begin() as conn:
-			await conn.run_sync(Base.metadata.create_all)
+    @staticmethod
+    async def create_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
-	@staticmethod
-	async def insert_user(username: str, password: str):
-		async with async_session_factory() as session:
-			new_user = User(username=username, password=password)
-			session.add(new_user)
-			await session.commit()
-			await session.refresh(new_user)
-			return new_user
+    @staticmethod
+    async def insert_user(username: str, password: str):
+        async with async_session_factory() as session:
+            new_user = User(username=username, password=password)
+            session.add(new_user)
+            await session.commit()
+            await session.refresh(new_user)
+            return new_user
 
-	@staticmethod
-	async def check_user(username: str, password: str):
-		async with async_session_factory() as session:
-			query = select(User).where(User.username == username)
-			result = await session.execute(query)
-			user = result.scalar_one_or_none()
-			if not user or user.password != password:
-				return None
-			return user
+    @staticmethod
+    async def check_user(username: str, password: str):
+        async with async_session_factory() as session:
+            query = select(User).where(User.username == username)
+            result = await session.execute(query)
+            user = result.scalar_one_or_none()
+            if not user or user.password != password:
+                return None
+            return user
 
-	@staticmethod
-	async def insert_task(input_data: str, user_id: int):
-		async with async_session_factory() as session:
-			new_task = Task(input_data=input_data, user_id=user_id)
-			session.add(new_task)
-			await session.commit()
-			await session.refresh(new_task)
-			return new_task
+    @staticmethod
+    async def insert_task(input_data: str, user_id: int):
+        async with async_session_factory() as session:
+            new_task = Task(input_data=input_data, user_id=user_id)
+            session.add(new_task)
+            await session.commit()
+            await session.refresh(new_task)
+            return new_task
 
-	@staticmethod
-	async def get_tasks(user_id: int):
-		async with async_session_factory() as session:
-			query = select(Task).filter(Task.user_id == user_id)
-			result = await session.execute(query)
-			tasks = result.scalars().all()
-			return tasks
+    @staticmethod
+    async def get_tasks(user_id: int):
+        async with async_session_factory() as session:
+            query = select(Task).filter(Task.user_id == user_id)
+            result = await session.execute(query)
+            tasks = result.scalars().all()
+            return tasks
 
-	@staticmethod
-	async def get_one_task(task_id: int):
-		async with async_session_factory() as session:
-			query = select(Task).filter_by(id=task_id)
-			result = await session.execute(query)
-			task = result.scalar_one_or_none()
-			return task
+    @staticmethod
+    async def get_one_task(task_id: int):
+        async with async_session_factory() as session:
+            query = select(Task).filter_by(id=task_id)
+            result = await session.execute(query)
+            task = result.scalar_one_or_none()
+            return task
 
-	@staticmethod
-	async def update_task(idx: int, status: str, res: str):
-		async with async_session_factory() as session:
-			query = select(Task).filter(Task.id == idx)
-			result = await session.execute(query)
-			task = result.scalar_one_or_none()
-			task.status = status
-			task.result = res
-			await session.commit()
+    @staticmethod
+    async def update_task(idx: int, status: str, res: str):
+        async with async_session_factory() as session:
+            query = select(Task).filter(Task.id == idx)
+            result = await session.execute(query)
+            task = result.scalar_one_or_none()
+            task.status = status
+            task.result = res
+            await session.commit()
--- backend/src/internal/routes.py
+++ backend/src/internal/routes.py
@@ -1,8 +1,6 @@
-import asyncio
 from typing import List
 
-from fastapi import status, APIRouter, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import status, APIRouter, HTTPException, BackgroundTasks
 from src.DB.commands import AsyncORM
 from src.internal import schemas
 from src.ML.executor import TaskExecutor
@@ -12,39 +10,39 @@ router = APIRouter(prefix="/api", tags=["api"])
 
 @router.on_event("startup")
 async def startup():
-	await AsyncORM.create_tables()
+    await AsyncORM.create_tables()
 
 
 @router.post("/users/register", response_model=schemas.User)
 async def create_user(user: schemas.UserCreate):
-	new_user = await AsyncORM.insert_user(user.username, user.password)
-	return new_user
+    new_user = await AsyncORM.insert_user(user.username, user.password)
+    return new_user
 
 
 @router.post("/users/login")
 async def login(user: schemas.UserLogin):
-	if result := await AsyncORM.check_user(user.username, user.password):
-		return schemas.User.model_validate(result)
-	raise HTTPException(
-		status_code=status.HTTP_401_UNAUTHORIZED,
-		detail="Incorrect username or password",
-	)
+    if result := await AsyncORM.check_user(user.username, user.password):
+        return schemas.User.model_validate(result)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+    )
 
 
-@router.post("/tasks", response_model=schemas.Task)
-async def create_task(task: schemas.TaskCreate):
-	new_task = await AsyncORM.insert_task(task.input_data, task.user_id)
-	await TaskExecutor(new_task).execute()
-	return new_task
+@router.post("/tasks")
+async def create_task(task: schemas.TaskCreate, background_tasks: BackgroundTasks):
+    new_task = await AsyncORM.insert_task(task.input_data, task.user_id)
+    background_tasks.add_task(TaskExecutor(new_task).execute)
+    return {"result": "task successfully created"}
 
 
 @router.get("/tasks", response_model=List[schemas.Task])
 async def get_tasks(user_id: int):
-	tasks = await AsyncORM.get_tasks(user_id)
-	return tasks
+    tasks = await AsyncORM.get_tasks(user_id)
+    return tasks
 
 
 @router.get("/tasks/{task_id}", response_model=schemas.Task)
 async def get_task(task_id: int):
-	task = await AsyncORM.get_one_task(task_id)
-	return task
+    task = await AsyncORM.get_one_task(task_id)
+    return task
