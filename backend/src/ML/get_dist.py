252cf903d5ffff891ac3705d30e35bd89d0b185f
MaximIgitov <127975266+MaximIgitov@users.noreply.github.com>
2024-09-07 13:46:42 +0300

    add calculate_distance_stats

--- backend/src/ML/get_dist.py
+++ backend/src/ML/get_dist.py
@@ -19,6 +19,19 @@ def calculate_distance(region1, region2, file_path=r'utils\reg_coords.json'):
     return distance
 
 
-# # Пример использования
-# distance = calculate_distance(1, 2)
-# print(f"Расстояние: {distance}")
+def calculate_distance_stats(regions: list, target_region: int) -> tuple:
+    if not regions:
+        raise ValueError("Список регионов не может быть пустым")
+
+    distances = [calculate_distance(region, target_region) for region in regions]
+    min_distance = min(distances)
+    avg_distance = sum(distances) / len(distances)
+
+    return min_distance, avg_distance
+
+
+# Пример использования
+# regions = [1, 2]
+# target_region = 3
+# min_distance, avg_distance = calculate_distance_stats(regions, target_region)
+# print(f"Тест 1: Минимальное расстояние: {min_distance}, Среднее расстояние: {avg_distance}")
